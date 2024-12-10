596281a0f7197872dcb9e5bd81f4f384a324cbbd
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-12-10 17:09:17 -0300

    feat(1/0h32): update button shadcn styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className='p-10'>
-      Hello World
-      <Button variant='ghost'>click me </Button>
+    <div className='p-5 flex gap-4'>
+      <Button size={"lg"}>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"teritary"}>Teritary</Button>
     </div>
   );
 }
