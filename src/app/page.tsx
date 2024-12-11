2bbdbf2af77deafe97e1b377b5e3c9e040a91cc5
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-12-11 07:56:56 -0300

    fix: corrigindo erros nos componentes shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export default function Home() {
   return (
-    <div className='flex p-2 flex-col'>
+    <div className='flex p-2 gap-3 flex-col'>
+      <Select>
+        <SelectTrigger className='w-[180px]'>
+          <SelectValue placeholder='Theme' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value='light'>Light</SelectItem>
+          <SelectItem value='dark'>Dark</SelectItem>
+          <SelectItem value='system'>System</SelectItem>
+        </SelectContent>
+      </Select>
       <Input />
       <div className='p-5 flex items-center gap-4'>
         .<Button size={"lg"}>Primary</Button>
