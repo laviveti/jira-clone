f9c5aaa354177b388fcde8d7de27dd303a2b3c74
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-12-11 07:30:24 -0300

    refactor: alterando altura do input shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className='p-5 flex gap-4'>
-      <Button size={"lg"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritary"}>Teritary</Button>
+    <div className='flex p-2 flex-col'>
+      <Input />
+      <div className='p-5 flex items-center gap-4'>
+        .<Button size={"lg"}>Primary</Button>
+        <Button variant={"secondary"}>Secondary</Button>
+        <Button variant={"destructive"}>Destructive</Button>
+        <Button variant={"ghost"}>Ghost</Button>
+        <Button variant={"muted"}>Muted</Button>
+        <Button variant={"outline"}>Outline</Button>
+        <Button variant={"tertiary"}>Tertiary</Button>
+      </div>
     </div>
   );
 }
