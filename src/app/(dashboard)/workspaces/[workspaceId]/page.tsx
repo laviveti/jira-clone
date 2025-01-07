3d41f85a4086ff820ba5853a862a1efe381c95eb
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-07 10:28:03 -0300

    feat(1/5h44): criando WorkspaceIdPage

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkspaceIdPage() {
+  return (
+    <div>
+      <h1>Workspace Id Page</h1>
+    </div>
+  );
+}
--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -4,9 +4,17 @@ import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { RiAddCircleFill } from "react-icons/ri";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 export const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (workspaceId: string) => {
+    router.push(`/workspaces/${workspaceId}`);
+  };
 
   return (
     <div className='flex flex-col gap-y-2'>
@@ -14,7 +22,7 @@ export const WorkspaceSwitcher = () => {
         <p className='text-xs uppercase text-neutral-500'>Workspaces</p>
         <RiAddCircleFill className='size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition' />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className='w-full bg-neutral-200 font-medium p-1'>
           <SelectValue placeholder='No workspace selected' />
         </SelectTrigger>
