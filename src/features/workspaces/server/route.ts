ac280419a394e96f873b1c1c64e742adb50c2319
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-07 08:17:02 -0300

    feat(1/5h32): assegurando que o usuário visualize apenas os workspaces que possui

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,13 +2,28 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
-    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID);
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [Query.equal("userId", user.$id)]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID, [
+      Query.orderDesc("$createdAt"),
+      Query.contains("$id", workspaceIds),
+    ]);
+
     return c.json({ data: workspaces });
   })
   .post("/", zValidator("form", createWorkspaceSchema), sessionMiddleware, async (c) => {
@@ -31,6 +46,12 @@ const app = new Hono()
       name,
       userId: user.$id,
       imageUrl: uploadedImageUrl,
+    });
+
+    await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+      userId: user.$id,
+      workspaceId: workspace.$id,
+      role: MemberRole.ADMIN,
     });
 
     return c.json({ data: workspace });
