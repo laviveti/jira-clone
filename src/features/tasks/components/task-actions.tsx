76deb8043252b3d0de05a8b816ef42eb96f3edb6
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-14 16:52:31 -0300

    feat(2/3h07): tasks - compondo TaskActions

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -1,5 +1,5 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { ExternalLinkIcon } from "lucide-react";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
 
 interface TaskActionsProps {
   id: string;
@@ -13,9 +13,21 @@ export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
         <DropdownMenuContent align='end' className='w-48'>
-          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium p-[10px]'>
-            <ExternalLinkIcon className='size-4 mr-2 stroke-2' />
+          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+            <ExternalLinkIcon className='size-4 stroke-2' />
             Task Details
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+            <ExternalLinkIcon className='size-4 stroke-2' />
+            Open Project
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+            <PencilIcon className='size-4 stroke-2' />
+            Edit Task
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => {}} disabled={false} className='text-amber-700 focus:text-amber-700 font-medium text-sm p-2'>
+            <TrashIcon className='size-4 stroke-2' />
+            Delete Task
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
