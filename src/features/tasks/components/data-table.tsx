7e8cfdaabedd677a9102311ad763dc7c009eb8a8
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-14 16:11:26 -0300

    feat(2/3h05): tasks - iniciando componente TaskActions

--- src/features/tasks/components/data-table.tsx
+++ src/features/tasks/components/data-table.tsx
@@ -12,7 +12,6 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 interface DataTableProps<TData, TValue> {
