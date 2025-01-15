f7e4177f817af80e11c979a8cf09a0bf28056bd9
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-15 08:01:14 -0300

    feat(2/3h14): tasks - deleção de tarefas direto da tabela

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -1,5 +1,8 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
 
 interface TaskActionsProps {
   id: string;
@@ -8,24 +11,39 @@ interface TaskActionsProps {
 }
 
 export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+  const router = useRouter();
+
+  const [ConfirmDialog, confirm] = useConfirm("Delete Task", "This action cannot be undone.", "destructive");
+  const { mutate: deleteTask, isPending: isDeletingTask } = useDeleteTask();
+
+  const onDelete = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+    deleteTask({ param: { taskId: id } });
+  };
+
   return (
     <div className='flex justify-end'>
+      <ConfirmDialog />
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
         <DropdownMenuContent align='end' className='w-48'>
-          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+          <DropdownMenuItem onClick={() => {}} className='font-medium text-sm p-2'>
             <ExternalLinkIcon className='size-4 stroke-2' />
             Task Details
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+          <DropdownMenuItem onClick={() => {}} className='font-medium text-sm p-2'>
             <ExternalLinkIcon className='size-4 stroke-2' />
             Open Project
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => {}} disabled={false} className='font-medium text-sm p-2'>
+          <DropdownMenuItem onClick={() => {}} className='font-medium text-sm p-2'>
             <PencilIcon className='size-4 stroke-2' />
             Edit Task
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => {}} disabled={false} className='text-amber-700 focus:text-amber-700 font-medium text-sm p-2'>
+          <DropdownMenuItem
+            onClick={onDelete}
+            disabled={isDeletingTask}
+            className='text-amber-700 focus:text-amber-700 font-medium text-sm p-2'>
             <TrashIcon className='size-4 stroke-2' />
             Delete Task
           </DropdownMenuItem>
