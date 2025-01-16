31206842aa3c69b20cb54808c2faee883a67dbac
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-16 07:54:02 -0300

    feat(2/3h50): tasks - implementando DataKanban

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -17,10 +17,12 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       <CreateTaskModal />
       <EditTaskModal />
       <div className='flex w-full h-full'>
-        <div className='fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-auto'>
+        {/* <div className='fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-auto'> */}
+        <div className='fixed left-0 top-0 hidden lg:block lg:w-[240px] h-full overflow-auto'>
           <Sidebar />
         </div>
-        <div className='lg:pl-[264px] w-full'>
+        <div className='lg:pl-[240px] w-full'>
+          {/* <div className='lg:pl-[264px] w-full'> */}
           <div className='mx-auto max-w-screen-2xl h-full'>
             <Navbar />
             <main className='h-full py-8 px-6 flex flex-col'>{children}</main>
