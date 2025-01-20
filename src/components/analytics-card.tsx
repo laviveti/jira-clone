0a8c641537b4ad63974a6cdabd8cf415323d7769
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-01-20 08:32:13 -0300

    feat(2/6h36): implementando componente Analytics

--- src/components/analytics-card.tsx
+++ src/components/analytics-card.tsx
@@ -22,7 +22,12 @@ export const AnalyticsCard = ({ title, value, variant, increaseValue }: Analytic
           <CardDescription className='flex items-center gap-x-2 font-medium overflow-hidden'>
             <span className={"truncate text-base"}>{title}</span>
           </CardDescription>
+          <div className='flex items-center gap-x-1'>
+            <Icon className={cn(iconColor, "size-4")} />
+            <span className={cn(increaseValueColor, "truncate text-base font-medium")}>{increaseValue}</span>
+          </div>
         </div>
+        <CardTitle className='text-3xl font-semibold'>{value}</CardTitle>
       </CardHeader>
     </Card>
   );
--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { AnalyticsCard } from "./analytics-card";
+import { DottedSeparator } from "./dotted-separator";
 
 export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
   return (
@@ -9,13 +10,50 @@ export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
       <div className='w-full flex flex-row'>
         <div className='flex items-center flex-1'>
           <AnalyticsCard
-            title='Total tasks'
+            title='Total Tasks'
             value={data.taskCount}
             variant={data.taskDifference > 0 ? "up" : "down"}
             increaseValue={data.taskDifference}
           />
+          <DottedSeparator direction='vertical' />
+        </div>
+        <div className='flex items-center flex-1'>
+          <AnalyticsCard
+            title='Assigned Tasks'
+            value={data.assignedTaskCount}
+            variant={data.assignedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.assignedTaskDifference}
+          />
+          <DottedSeparator direction='vertical' />
+        </div>
+        <div className='flex items-center flex-1'>
+          <AnalyticsCard
+            title='Completed Tasks'
+            value={data.completedTaskCount}
+            variant={data.completedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.completedTaskDifference}
+          />
+          <DottedSeparator direction='vertical' />
+        </div>
+        <div className='flex items-center flex-1'>
+          <AnalyticsCard
+            title='Overdue Tasks'
+            value={data.overdueTaskCount}
+            variant={data.overdueTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.overdueTaskDifference}
+          />
+          <DottedSeparator direction='vertical' />
+        </div>
+        <div className='flex items-center flex-1'>
+          <AnalyticsCard
+            title='Incomplete Tasks'
+            value={data.incompleteTaskCount}
+            variant={data.incompleteTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.incompleteTaskDifference}
+          />
         </div>
       </div>
+      <ScrollBar orientation='horizontal' />
     </ScrollArea>
   );
 };
